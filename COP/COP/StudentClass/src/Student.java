692a2d6f8ae5f235ecb8da4c73d5e4b8abe4ee4a
Rupesh_Kamble <rupeshkamble.1314@gmail.com>
2022-03-29 22:01:05 +0530

    Student class

---
+++ COP/COP/StudentClass/src/Student.java
@@ -0,0 +1,22 @@
+class date {
+	private int dd,mm,yy;
+	
+	public Date()
+	{
+		
+	}
+}
+
+
+
+
+
+public class Student {
+	private int rno;
+	private String name;
+	private float marks;
+	private int age;
+	private Date doj;
+	
+	public Student() {}
+}
